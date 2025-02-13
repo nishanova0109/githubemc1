0bc0507aa363f21e20663ce2ebf530d271319413
nishanova0109 <nishanova0109@gmail.com>
2025-02-13 20:37:12 +0530

    adding code

---
+++ diamondshape2.py
@@ -0,0 +1,18 @@
+for i in range(6):
+    for j in range (6-i):
+        print(" ",end="")
+    for k in range (1,i):
+        print(k,end="")
+    for m in range(i,0,-1):
+        print(m,end="")
+        
+        
+    print()
+for i in range (4,0,-1):
+   for j in range (6-i):
+        print(" ",end="")
+   for k in range (1,i):
+        print(k,end="")
+   for m in range(i,0,-1):
+        print(m,end="")
+   print()
