5f82ad82c338091f30a2271f1346c136fc547fe8
nishanova0109 <Nishanova0109@gmail.com>
2025-03-19 21:26:42 +0530

    Add files via upload

---
+++ 19.03.25.java
@@ -0,0 +1,35 @@
+public class Solution {
+    public int removeElement(int[] nums, int val) {
+        int slow = 0;
+        for (int fast = 0; fast < nums.length; fast++) {
+            if (nums[fast] != val) {
+                nums[slow] = nums[fast];
+                slow++;
+            }
+        }
+        return slow;
+    }
+}
+
+
+public class Solution {
+    public int firstMissingPositive(int[] nums) {
+        int n = nums.length;
+        
+        for (int i = 0; i < n; i++) {
+            while (nums[i] > 0 && nums[i] <= n && nums[nums[i] - 1] != nums[i]) {
+                int temp = nums[i];
+                nums[i] = nums[temp - 1];
+                nums[temp - 1] = temp;
+            }
+        }
+        
+        for (int i = 0; i < n; i++) {
+            if (nums[i] != i + 1) {
+                return i + 1;
+            }
+        }
+        
+        return n + 1;
+    }
+}
